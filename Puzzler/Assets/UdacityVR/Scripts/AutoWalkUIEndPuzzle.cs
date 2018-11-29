e2ed88847c046d39fcee0661de6678c0141ac320
Mansoor Patan <1955mars@gmail.com>
2018-11-29 23:07:09 +0530

    Puzzler
         - VR version of Simon Says game

---
+++ Puzzler/Assets/UdacityVR/Scripts/AutoWalkUIEndPuzzle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AutoWalkUIEndPuzzle : MonoBehaviour, IGvrPointerHoverHandler // Implement Interface for Gaze hoover method
+{
+    public Slider slider;
+    public float gazeTime = 2.0f;
+    public GameLogic gameLogic;
+    private bool locked;
+
+    public void Start()
+    {
+        slider.gameObject.SetActive(false);
+        slider.value = 0f;
+        slider.maxValue = gazeTime;
+        locked = false;
+    }
+
+    public void EnterButton()
+    {
+        slider.gameObject.SetActive(true);
+        slider.value = 0f;
+        locked = false;
+    }
+
+    public void LeavesButton()
+    {
+        slider.gameObject.SetActive(false);
+        slider.value = 0f;
+        locked = false;
+    }
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("OnGVRPointerHover called");
+        if (!locked)
+        {
+            slider.value += Time.deltaTime;
+
+            if (slider.value >= gazeTime)
+            {
+                slider.value = 0f;
+                gameLogic.ResetPuzzle();
+                locked = true;
+            }
+        }
+    }
+}
---
+++ Puzzler/Assets/UdacityVR/Scripts/AutoWalkUISelect.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AutoWalkUISelect : MonoBehaviour, IGvrPointerHoverHandler // Implement Interface for Gaze hoover method
+{
+    public Canvas canvas;
+    public Slider slider;
+    public float gazeTime = 2.0f;
+    public LightUp lightUp;
+    private bool locked;
+
+    public void Start()
+    {
+        canvas.gameObject.SetActive(false);
+        slider.value = 0f;
+        slider.maxValue = gazeTime;
+        locked = false;
+    }
+
+    public void EnterButton()
+    {
+        canvas.gameObject.SetActive(true);
+        slider.value = 0f;
+        locked = false;
+    }
+
+    public void LeavesButton()
+    {
+        canvas.gameObject.SetActive(false);
+        slider.value = 0f;
+        locked = false;
+    }
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("OnGVRPointerHover called");
+        if (!locked)
+        {
+            slider.value += Time.deltaTime;
+
+            if (slider.value >= gazeTime)
+            {
+                slider.value = 0f;
+                lightUp.PlayerSelection();
+                locked = true;
+            }
+        }
+    }
+}
